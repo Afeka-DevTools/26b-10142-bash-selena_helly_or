27aebf71dfdc91f57fd32df6a63142659933de2e
regenn95 <orkline1995@gmail.com>
2026-06-20 17:59:51 +0300

    Added Prefix To Filename Script

---
+++ scripts/add_prefix.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+prefix="in"
+target_dir="$(cd "$(dirname "$0")/prefix_test" && pwd)"
+
+add_prefix() {
+	for file in "$target_dir"/*.txt; do
+		[ -e "$file" ] || continue
+
+		filename="$(basename "$file")"
+
+		case "$filename" in
+			"$prefix"*)
+				echo "$filename already has the prefix"
+				continue
+				;;
+		esac
+
+		mv "$file" "$target_dir/$prefix$filename"
+		echo "$filename renamed to $prefix$filename"
+	done
+}
+
+add_prefix
